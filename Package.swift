c438c182b127db7232d0adb00b55007370a1ef70
Mohammad Rahchamani <mohammad.rahchamani@resonanzgroup.com>
2024-08-16 15:52:49 +0400

    update path

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .target(
             name: "MMNumberKeyboard",
             path: "Classes",
-            resources: [.copy("../Images")],
+            resources: [.copy("Images")],
             publicHeadersPath: "."
             ),
     ]
